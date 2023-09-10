8ddf87a36d4f8f4d11ac393feacd69ef3ba89546
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-09-10 23:28:37 +0530

    Create Reverse a stack.java

---
+++ Reverse a stack.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+public class revstack{
+    public static void reverse(Stack<Integer> st){
+    Stack<Integer> auxstack=new Stack<>();
+    while(!st.isEmpty()){
+        int item=st.pop();
+        auxstack.push(item);
+    }
+    while(!auxstack.isEmpty()){
+        int item=auxstack.pop();
+        st.push(item);
+    }
+    }
+    public static void main(String[] args){
+        Stack <Integer> st=new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+        reverse(st);
+        System.out.println(st);
+    }
+}
