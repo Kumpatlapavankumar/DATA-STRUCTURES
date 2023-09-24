8d4c8544108a8c69cf32556df4be1f5802084ecf
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-09-24 18:50:14 +0530

    Create Implementation of Queue using LinkedList.java

---
+++ Implementation of Queue using LinkedList.java
@@ -0,0 +1,70 @@
+class Queue<T> {
+    private Node<T> front;
+    private Node<T> rear;
+
+    // Inner class to represent a linked list node
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Constructor to initialize the queue
+    public Queue() {
+        front = null;
+        rear = null;
+    }
+
+    // Check if the queue is empty
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    // Get the size of the queue
+    public int size() {
+        int count = 0;
+        Node<T> current = front;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    // Add an element to the rear of the queue
+    public void enqueue(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (isEmpty()) {
+            front = newNode;
+            rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+    }
+
+    // Remove and return an element from the front of the queue
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        T data = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+        return data;
+    }
+
+    // Peek at the element at the front of the queue without removing it
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return front.data;
+    }
+}
