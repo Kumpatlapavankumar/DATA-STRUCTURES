2a2facb97372b52dc7d5a7ebfe4112e0b12069ef
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-10-15 17:23:33 +0530

    Update Array of Contiguous Integers problem.java

--- Array of Contiguous Integers problem.java
+++ Array of Contiguous Integers problem.java
@@ -1,31 +1,32 @@
-import  java.util.*;
-public class ContiguousIntegers {
+import java.util.*;
+public class ContiguousIntegers{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] arr=new int[n];
         for(int i=0;i<n;i++){
-            arr[i]= sc.nextInt();
+            arr[i]=sc.nextInt();
         }
         HashSet<Integer> set=new HashSet<>();
+        boolean found=false;
         for(int num:arr){
             set.add(num);
         }
-        int maxlenth=0;
-        int start=0;
-        for(int num:arr){
-            int current=0;
-            while (set.contains(num+current)){
-                current++;
-            }
-            if(current>maxlenth){
-                maxlenth=current;
-                start=num;
-            }
+        int current=arr[0];
+        int count=0;
+        while(set.contains(current)==true){
+                count++;
+                current--;
         }
-        System.out.println("longest contigues sub array:");
-        for(int i=start;i<maxlenth;i++){
-            System.out.println(i+" ");
+        current=arr[0]+1;
+        while(set.contains(current)==true){
+            count++;
+            current++;
+        }
+        if(count==n){
+            System.out.println("true");
+        }else{
+            System.out.println("Flase");
         }
     }
 }
