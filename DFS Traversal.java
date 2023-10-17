30d18dd71aeea42c51494c28eb5e4c10c9d51389
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-10-17 21:21:37 +0530

    Create DFS Traversal.java

---
+++ DFS Traversal.java
@@ -0,0 +1,57 @@
+class Node{
+    int data;
+    Node left,right;
+    Node(int value){
+        data=value;
+        left=right=null;
+    }
+}
+class Tree{
+    Node root;
+    Tree(){
+        root=null;
+    }
+    void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+}
+public class DFS {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.left.left.right = new Node(6);
+        tree.root.right.left = new Node(7);
+        tree.root.right.right = new Node(8);
+        System.out.println("========preorder=========");
+        tree.preorder(tree.root);
+        System.out.println("\n========Inorder=========");
+        tree.inorder(tree.root);
+        System.out.println("\n========postorder=========");
+        tree.postorder(tree.root);
+    }
+}
