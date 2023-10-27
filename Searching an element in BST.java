dfb1e648bcbb4b7689e7d368b6e38b976b0070d9
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-10-27 21:38:58 +0530

    Create Searching an element in BST.java

---
+++ Searching an element in BST.java
@@ -0,0 +1,40 @@
+class Node{
+    int data;
+    Node left,right;
+    Node(int value){
+        data=value;
+        left=right=null;
+    }
+}
+class Tree{
+    Node root;
+    Tree(){
+        root=null;
+    }
+    boolean searching(Node root,int key){
+        if(root==null){
+            return false;
+        }
+        if(root.data==key){
+            return true;
+        }
+        else if(root.data>key){
+            return searching(root.left,key);
+        }else {
+            return searching(root.right,key);
+        }
+    }
+}
+public class  searchingofBST{
+    public static void main(String[] args){
+        Tree tree=new Tree();
+        tree.root=new Node(100);
+        tree.root.left=new Node(70);
+        tree.root.right=new Node(120);
+        tree.root.left.left=new Node(67);
+        tree.root.left.right=new Node(72);
+        tree.root.right.left=new Node(110);
+        tree.root.left.left.left=new Node(60);
+        System.out.print(tree.searching(tree.root,600));
+    }
+}
