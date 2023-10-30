40bb996811ff59b95695d7eabc5ed94cef26001c
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-10-30 18:29:40 +0530

    Create Creategraph.java

---
+++ Creategraph.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class creategraph{
+    public static class Edge{
+        int source;
+        int destination;
+        public Edge(int source,int destination){
+            this.source=source;
+            this.destination=destination;
+        }
+    }
+    public static void creategraph(ArrayList<Edge>[] graph){
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+        graph[6].add(new Edge(6, 5));
+    }
+    public static void main(String[] args){
+        int v=7;
+        ArrayList<Edge>[] graph=new ArrayList[v];
+        creategraph(graph);
+    }
+}
