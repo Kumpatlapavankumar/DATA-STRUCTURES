2b4eabc9fa4ad268191b0415eaf774fead3cefd5
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-11-10 00:38:25 +0530

    Create First Unique Character problem.java

---
+++ First Unique Character problem.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class firstuniquecharacter{
+    public static void main(String[] args) {
+        String sc="qadvdffdfs";
+        Character uniquecharacter=null;
+        Map<Character,Integer> h=new LinkedHashMap();
+        for(Character ch:sc.toCharArray()){
+            if(!h.containsKey(ch)){
+                h.put(ch,1);
+            }else{
+                h.put(ch,h.get(ch)+1);
+            }
+        }
+        for(Character ch:sc.toCharArray()){
+            if(h.get(ch)==1){
+                uniquecharacter=ch;
+                break;
+            }
+        }
+        if(uniquecharacter==null){
+            System.out.println("no unique characters");
+        }else{
+            System.out.println(uniquecharacter);
+        }
+    }
+}
