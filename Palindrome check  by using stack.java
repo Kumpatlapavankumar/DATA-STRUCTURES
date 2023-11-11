8fba148db1d75f177a01248ac572ca6f97a63127
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-11-11 20:48:05 +0530

    Create Palindrome check  by using stack.java

---
+++ Palindrome check  by using stack.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class pailndrome{
+    public static void main(String[] args){
+        String name="madkam";
+        pailndromematching(name);
+    }
+    public static void pailndromematching(String name){
+        Queue<Character> queue=new LinkedList<>();
+        for(int i=name.length()-1;i>=0;i--){
+            queue.add(name.charAt(i));
+        }
+        String reverse="";
+        while(!queue.isEmpty()){
+            reverse=reverse+queue.remove();
+        }
+        if(name.equals(reverse)){
+            System.out.println("palindrome");
+        }else{
+            System.out.println("Not palindrome");
+        }
+    }
+}
