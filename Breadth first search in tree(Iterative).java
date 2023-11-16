83e711ce6e7e20bdcb320507819a1b0f7cccc3e0
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-11-16 22:04:23 +0530

    Update and rename BFS(Iterative).java to Breadth first search in tree(Iterative).java

--- Breadth first search in tree(Iterative).java
+++ Breadth first search in tree(Iterative).java
@@ -1,39 +1,4 @@
 
-/*class Tree{
-    Node root;
-    Tree(){
-        root=null;
-    }
-    void Bfstree(){
-        Queue<Node> q=new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            Node temp=q.peek();
-            q.remove();
-            System.out.println(temp.data);
-            if(temp.left!=null){
-                q.add(temp.left);
-            }
-            if(temp.right!=null){
-                q.add(temp.right);
-            }
-        }
-    }
-}
-public class BFSIterative {
-    public static void main(String[] args){
-        Tree tree=new Tree();
-        tree.root=new Node(1);
-        tree.root.left=new Node(2);
-        tree.root.right=new Node(3);
-        tree.root.left.left=new Node(4);
-        tree.root.left.right=new Node(5);
-        tree.root.left.left.right=new Node(6);
-        tree.root.right.left=new Node(7);
-        tree.root.right.right=new Node(8);
-        tree.Bfstree();
-    }
-}*/
 import java.util.*;
 
 class Node{
