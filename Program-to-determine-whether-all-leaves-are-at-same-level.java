f326645b4b2cf18c395251613831d1a5f5f802d4
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-12-17 22:06:16 +0530

    Update Program-to-determine-whether-all-leaves-are-at-same-level.java

--- Program-to-determine-whether-all-leaves-are-at-same-level.java
+++ Program-to-determine-whether-all-leaves-are-at-same-level.java
@@ -1,20 +1,16 @@
-public class LeafLevel {  
-  
-    //Represent a node of binary tree  
+public class LeafLevel {    
     public static class Node{  
         int data;  
         Node left;  
         Node right;  
   
         public Node(int data){  
-            //Assign data to the new node, set left and right children to null  
             this.data = data;  
             this.left = null;  
             this.right = null;  
             }  
         }  
-  
-    //Represent the root of binary tree  
+ 
     public Node root;  
   
     //It will store level of first encountered leaf  
