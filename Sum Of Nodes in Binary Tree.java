e9016c3d8ed4b0ef7761fd689a3eacc17ce96617
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-06-04 22:04:17 +0530

    Update Sum Of Nodes in Binary Tree.java

--- Sum Of Nodes in Binary Tree.java
+++ Sum Of Nodes in Binary Tree.java
@@ -5,7 +5,6 @@ public class SumOfNodes {
         Node right;  
   
         public Node(int data){  
-            //Assign data to the new node, set left and right children to null  
             this.data = data;  
             this.left = null;  
             this.right = null;  
