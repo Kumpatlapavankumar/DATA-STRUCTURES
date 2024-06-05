a7a6ab112418148b957e6e9d88a1827104e33437
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-06-05 21:04:57 +0530

    Update Ticket booking system using queues.java

--- Ticket booking system using queues.java
+++ Ticket booking system using queues.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-
+//Ticket booking project 
 class Ticket {
     private String passengerName;
     private int seatNumber;
